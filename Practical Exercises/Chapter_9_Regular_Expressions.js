78bf1120bf1118a64cf19fcabacd2e4eedbbeb73
Galina Karateeva <karateewa.galina@yandex.ru>
2021-01-04 21:25:12 +0100

    Solutions to Chapter 9 (Regular Expressions) Exercises.

---
+++ Practical Exercises/Chapter_9_Regular_Expressions.js
@@ -0,0 +1,67 @@
+//First
+
+verify(/ca[rt]/,
+    ["my car", "bad cats"],
+    ["camper", "high art"]);
+
+verify(/pr?op/,
+    ["pop culture", "mad props"],
+    ["plop", "prrrop"]);
+
+verify(/ferr(et|y|ari)/,
+    ["ferret", "ferry", "ferrari"],
+    ["ferrum", "transfer A"]);
+
+verify(/ious\b/,
+    ["how delicious", "spacious room"],
+    ["ruinous", "consciousness"]);
+
+verify(/\s[,.:;]/,
+    ["bad punctuation ."],
+    ["escape the period"]);
+
+verify(/\w{7,}/,
+    ["Siebentausenddreihundertzweiundzwanzig"],
+    ["no", "three small words"]);
+
+verify(/\b[^\We]+\b/i,
+    ["red platypus", "wobbling nest"],
+    ["earth bed", "learning ape", "BEET"]);
+
+
+function verify(regexp, yes, no) {
+    // Ignore unfinished exercises
+    if (regexp.source == "...") return;
+    for (let str of yes) if (!regexp.test(str)) {
+        console.log(`Failure to match '${str}'`);
+    }
+    for (let str of no) if (regexp.test(str)) {
+        console.log(`Unexpected match for '${str}'`);
+    }
+}
+
+//Second
+let text = "'I'm the cook,' he said, 'it's my job.'";
+// Change this call.
+console.log(text.replace(/(^|\W)'|'(\W|$)/g, "$1\"$2"));
+// → "I'm the cook," he said, "it's my job."
+
+//Third
+let number = /^[+\-]?(\d+(\.\d*)?|\.\d+)(e[+\-]?\d+)?$/i;
+
+// Tests:
+for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
+    "1.3e2", "1E-4", "1e+12"]) {
+    if (!number.test(str)) {
+        console.log(`Failed to match '${str}'`);
+    }
+}
+for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
+    ".5.", "1f5", "."]) {
+    if (number.test(str)) {
+        console.log(`Incorrectly accepted '${str}'`);
+    }
+}
+
+
+
